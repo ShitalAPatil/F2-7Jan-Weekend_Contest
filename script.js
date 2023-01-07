54af1e142198bcb05a9eddb0ae6401ed15597d79
ShitalAPatil <shitalpatilb29@gmail.com>
2023-01-07 12:41:46 +0530

    first commit

--- script.js
+++ script.js
@@ -7,21 +7,49 @@ let arr = [
 ];
 
 function PrintDeveloperswithMap() {
-  //Write your code here
+  arr.map(printingThroghMap); // Each element is stored in "printingThroghMap" variable
+    function printingThroghMap(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here
+  arr.forEach(printingThroghForEach); // Each element is stored in "printingThroghForEach" variable
+
+  function printingThroghForEach(arrayItem) {
+    if (arrayItem.profession === "developer") {
+      console.log(arrayItem);
+    }
+  }
 }
 
 function addData() {
-  //Write your code here
+  let newObj = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newObj);
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here
+  let filtered_arr = arr.filter(function (val) {
+    //callback function
+    if (val.profession !== "admin") {
+      //filtering criteria
+      return val;
+    }
+  });
+  console.log(filtered_arr);
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 5, name: "siya", age: "22", profession: "Intern" },
+    { id: 6, name: "ankita", age: "26", profession: "developer" },
+    { id: 7, name: "veera", age: "29", profession: "Manager" },
+  ];
+
+  let concatArray = arr.concat(arr2);
+  console.log(concatArray);
 }
